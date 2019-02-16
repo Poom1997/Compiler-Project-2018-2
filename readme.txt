dacb8fad68758f614e92360ca398f609fffd0037
Bhurinuth Wongsrisakul <poom1997@gmail.com>
2019-02-16 17:33:10 +0700

    create generator file

---
+++ readme.txt
@@ -0,0 +1,7 @@
+1) Please run the scanner_generator.py
+2) Input the filename of the regex expression
+
+**NOTE: PLEASE PUT THE REGEX FILE IN THE SAME DIRECTORY!**
+
+3) A scanner will be generated. 
+4) Please run the newly generated scanner 58090030_scanner_date_time.py and input the string to tokenize.
--- scanner_generator.py
+++ scanner_generator.py
@@ -1,16 +1,23 @@
 import datetime
 
-time = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
-fileName = "58090030_scanner_" + time+".py"
+def thomson_algorithm(regex):
+    pass
 
-automaton_template = "scanner_template"
-regex = "regex_list = [[{0,1,2,3},{'a','b'},{(0, 'a'): {0},(0, 'b'): {1},(1, 'a'): {2},(2, 'b'): {3}},0,{3},'a*bab'],[{0, 1, 2},{'a', 'c'},{(0, 'a'): {1},(1, 'c'): {2},(2, ''): {0}},0,{2},'(ac)*']]"
+def generate_scanner(regex):
+    time = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+    fileName = "58090030_scanner_" + time+".py"
 
-template = open(automaton_template, 'r')
-out_file = open(fileName,'w')
+    automaton_template = "scanner_template.py"
 
-data_dump = regex
-data_dump = data_dump + '\n\n' + template.read()
+    template = open(automaton_template, 'r')
+    out_file = open(fileName,'w')
 
-out_file.write(data_dump)
-out_file.close()
+    data_dump = regex
+    data_dump = data_dump + '\n\n' + template.read()
+
+    out_file.write(data_dump)
+    out_file.close()
+
+if __name__ == '__main__':
+    regex = "regex_list = [[{0,1,2,3},{'a','b'},{(0, 'a'): {0},(0, 'b'): {1},(1, 'a'): {2},(2, 'b'): {3}},0,{3},'a*bab'],[{0, 1, 2},{'a', 'c'},{(0, 'a'): {1},(1, 'c'): {2},(2, ''): {0}},0,{2},'(ac)*']]"
+    generate_scanner(regex)
