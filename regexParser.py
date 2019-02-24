46e92804b847cef7f15bb899a3812c5f041d90ca
Poom Wongsrisakul <Poom1997@users.noreply.github.com>
2019-02-25 00:26:42 +0700

    re-implement parser

--- regexParser.py
+++ regexParser.py
@@ -10,97 +10,75 @@ class Stack:
     def pop(self):
         return self.data.pop()
 
-    def add_Start(self, value):
-        self.start.append(value)
-
-    def print_Start(self):
-        print(self.start)
-
-    def set_CurrentPos(self, value):
-        self.currentPos = value
-
     def print_value(self):
         print(self.data)
 
-def thompson_or(n):
+    def previous(self):
+        return self.data[-1]
+
+    def empty(self):
+        if(len(self.data) == 0):
+            return True
+        else:
+            return False
+
+operator_list = ['(', '|', '*', ')']
+operator_order = {'(' : 0, '_' : 1, '|' : 2, '*' : 3} #_ is concat
+
+def thompson(n):
+    pass
+
+def preprocess_Regex(regex):
     pass
 
 def formatter(list):
-    print(list)
+   pass
 
-def parse(regex):
-    stack = Stack()
-    regex = '(' + regex + ')'
-
-    iteration = 0
-    and_count = 0
-    depth = -1
-    main_depth = 0
-
-    main_list = []
-    temp_list = []
-
+def extract_alphabet(regex):
+    alphabet = ['']
     for character in regex:
-        iteration += 1
+        if(character in operator_list):
+            pass
+        else:
+            alphabet.append(character)
 
-        if(character == '('):
+    return alphabet
+
+def change_to_Postfix(regex):
+    stack = Stack()
+    output = []
+    for character in regex:
+        #print(character)
+        #stack.print_value()
+        if(character not in operator_list):
+            output.append(character)
+
+        elif(character == '('):
             stack.push(character)
-            stack.add_Start(iteration)
-            temp_list.append([])
-            depth += 1
 
-        elif(character == "|"):
-            temp = []
-            for i in range(0, and_count):
-                temp.append(stack.pop())
-            if(depth!=0):
-                temp_list[depth].append(temp[::-1])
-            else:
-                main_list.append(temp[::-1])
-                main_depth += 1
-            and_count = 0
-
-        elif(character == ")"):
-            temp = []
-            for i in range(0, and_count):
-                temp.append(stack.pop())
-                #print("TMP ", temp)
-            #print("TMP ", temp)
-            if(len(temp)>0):
-                if(depth != 0):
-                    #print(depth)
-                    temp_list[depth].append(temp[::-1])
-                    depth -= 1
-                    #print("TL", temp_list)
-                    and_count = 0
-                    stack.pop()
-                    #print("LS", len(temp_list))
-                    #print(main_list)
-                    #print(main_depth)
-                    if(main_depth == 0):
-                        main_list.append(temp_list.pop())
-                    else:
-                        main_list[main_depth].append(temp_list.pop())
-                    #print(main_list)
-                else:
-                    if(main_depth == 0):
-                        main_list.append(temp.pop())
-                    else:
-                        main_list[main_depth].append(temp.pop())
-                    main_depth += 1
+        elif(character == ')'):
+            temp = stack.pop()
+            while(temp != '('):
+                output.append(temp)
+                temp = stack.pop()
 
         else:
-            and_count += 1
+            while(not stack.empty() and (operator_order[stack.previous()] >= operator_order[character])):
+                output.append(stack.pop())
             stack.push(character)
 
-        #print(iteration)
-        #stack.print_value()
-    print(main_list)
+    while(not stack.empty()):
+        temp = stack.pop()
+        output.append(temp)
 
-    #print("TMP: " , temp_list)
-    #stack.print_Start()
+    return output
 
-
-    for each_element in main_list:
-        formatter(each_element)
+def parse(regex):
+    print(regex)
+    regex = change_to_Postfix(regex)
+    print(regex)
+    regex = ''.join(regex)
+    print(regex)
+    alphabet = extract_alphabet(regex)
+    print(alphabet)
 
